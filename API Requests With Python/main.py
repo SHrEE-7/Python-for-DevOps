6a847c115730eb23f482af57ef284d861784704a
Shree <shreekumarf@gmail.com>
2022-12-15 23:03:43 +0530

    API request

--- API Requests With Python/main.py
+++ API Requests With Python/main.py
@@ -1,7 +1,7 @@
 import requests
 res = requests.get("https://api.github.com/users/SHrEE-7/repos")
 my_repos = res.json()
-print(my_repos[0])
+# print(my_repos[0])/
 
-# for repository in my_repos:
-#     print(f"Repository Name:{repository['name']} --> Repository URL:{repository['url']}")
+for repository in my_repos:
+    print(f"Repository Name:{repository['name']} --> Repository URL:{repository['url']}")
