ac8304661ecd7ed9bea7ab1935f53be8f80cbe9c
SHrEE-7 <shreekumarf@gmail.com>
2024-01-08 22:55:28 +0530

    main.py

--- Connect to AWS/main.py
+++ Connect to AWS/main.py
@@ -2,7 +2,8 @@ import boto3
 
 ec2_client = boto3.client('ec2')
 all_available_vpcs = ec2_client.describe_vpcs()
-vpcs = all_available_vpcs["Vpcs"]
+print(all_available_vpcs)
+# vpcs = all_available_vpcs["Vpcs"]
 
-for vpc in vpcs:
+# for vpc in vpcs:
     
