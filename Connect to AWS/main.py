efa298b304c80f00f34d063df9daeebef0823584
SHrEE-7 <shreekumarf@gmail.com>
2024-01-09 23:00:42 +0530

    print data

--- Connect to AWS/main.py
+++ Connect to AWS/main.py
@@ -1,9 +1,11 @@
 import boto3
 
 ec2_client = boto3.client('ec2')
+# ec2_client = boto3.client('ec2', region_name = "ap-south-2")  // explicitly specify the region.
 all_available_vpcs = ec2_client.describe_vpcs()
-print(all_available_vpcs)
+# print(all_available_vpcs)
 vpcs = all_available_vpcs["Vpcs"]
 
-# for vpc in vpcs:
+for vpc in vpcs:
+    print(vpc["VpcId"])
     
