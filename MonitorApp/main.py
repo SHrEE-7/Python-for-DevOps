dd4c7465d03d6888ce4b4bde412b58c6d2c21b27
SHrEE-7 <shreekumarf@gmail.com>
2024-01-14 22:32:55 +0530

    monitor application

---
+++ MonitorApp/main.py
@@ -0,0 +1,26 @@
+import requests
+import smtplib
+import os
+
+EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+
+def send_notification(msg):
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.starttls()
+        smtp.ehlo()
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        smtp.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, msg)
+try:
+    response = requests.get('http://13.234.48.115/')
+    if response.status_code != 200:
+        print('Application is running Successfully')
+    else:
+        print('Application Down. Fix it')
+        msg = f"Application returned {response.status_code}"
+        send_notification(msg)
+
+except Exception as ex:
+    print(f'Connection error encountered: {ex}')
+    msg = "Application not accessible at all!!. Fix ASAP"
+    send_notification(msg)
