1750708d1c4167504e391a69424938dbc07057ae
SHrEE-7 <shreekumarf@gmail.com>
2024-01-14 20:24:53 +0530

    Create a snapshot of volume

---
+++ Create Volume Snapshot/main.py
@@ -0,0 +1,17 @@
+import boto3
+import schedule
+
+ec2_client = boto3.client('ec2')
+
+def create_volume_snapshot():
+    volumes = ec2_client.describe_volumes()
+    for volume in volumes['Volumes']:
+        new_snapshot = ec2_client.create_snapshot(
+            VolumeId = volume["VolumeId"]
+        )
+        print(new_snapshot)
+
+schedule.every().day.do(create_volume_snapshot)
+
+while True:
+    schedule.run_pending()
